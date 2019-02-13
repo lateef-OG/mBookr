7e5f1c35bca63d84408d4ac8b50c05037d954cde
lateef_og <lateef@cchubnigeria.com>
2019-02-13 15:01:21 +0200

    setup meal service to get all, add meal and get meal

--- Api/src/index.js
+++ Api/src/index.js
@@ -5,6 +5,24 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 app.use('/api/v1/meals', MealsRoute);
+app.use((req, res) => {
+  const error = new Error('Not found');
+  res.status(404);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+app.use((error, req, res) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 app.listen(PORT);
 
 export default app;
--- Api/src/routes/meals.js
+++ Api/src/routes/meals.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import MealService from '../services/meals.service';
+
+const mealService = new MealService();
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Handling get request to meals',
-  });
+  res.status(200).send(mealService.getAll());
 });
 
 export default router;
