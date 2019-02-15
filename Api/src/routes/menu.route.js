4fcdf9fa995ca6b59de109424d48ad75fbb08bad
lateef_og <lateef@cchubnigeria.com>
2019-02-15 18:50:37 +0200

    created route for menu and tested with postman

---
+++ Api/src/routes/menu.route.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import MenuController from '../controllers/menu.controller';
+
+const router = Router();
+router.get('/', MenuController.getMenu);
+router.post('/', MenuController.addMenu);
+
+export default router;
