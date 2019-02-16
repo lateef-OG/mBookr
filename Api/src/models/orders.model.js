691a68e2421eb45f67006381f5f287cb4dc4c4a6
lateef_og <lateef@cchubnigeria.com>
2019-02-16 02:52:31 +0200

    created order model

---
+++ Api/src/models/orders.model.js
@@ -0,0 +1,10 @@
+export default class Meal {
+  constructor() {
+    this.id = null;
+    this.customer_name = null;
+    this.meals = null;
+    this.address = null;
+    this.phone_no = null;
+    this.total_cost = null;
+  }
+}
