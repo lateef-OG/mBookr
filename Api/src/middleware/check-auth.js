e702e5b29f54232113a6d06319b1255cd4cf3509
lateef_og <lateef@cchubnigeria.com>
2019-03-05 19:52:37 +0100

    added authentication to protected routes

--- Api/src/middleware/check-auth.js
+++ Api/src/middleware/check-auth.js
@@ -2,6 +2,35 @@
 import jwt from 'jsonwebtoken';
 
 const checkAuth = {
+  all(req, res, next) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, 'secret');
+      req.userData = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        message: 'User not authorized',
+      });
+    }
+  },
+  customer(req, res, next) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, 'secret');
+      req.userData = decoded;
+      if (req.userData.role_id !== 1) {
+        return res.status(401).json({
+          message: 'User not authorized',
+        });
+      }
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        message: 'User not authorized',
+      });
+    }
+  },
   customer(req, res, next) {
     try {
       const token = req.headers.authorization.split(' ')[1];
--- Api/src/routes/menu.route.js
+++ Api/src/routes/menu.route.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import MenuController from '../controllers/menu.controller';
+import CheckAuth from '../middleware/check-auth';
 
 const router = Router();
-router.get('/', MenuController.fetchMenu);
-router.post('/', MenuController.addMeal);
-router.delete('/', MenuController.deleteMeal);
-router.delete('/:menu_id', MenuController.deleteMenu);
+router.get('/', CheckAuth.all, MenuController.fetchMenu);
+router.post('/', CheckAuth.caterer, MenuController.addMeal);
+router.delete('/', CheckAuth.caterer, MenuController.deleteMeal);
+router.delete('/:menu_id', CheckAuth.caterer, MenuController.deleteMenu);
 
 export default router;
